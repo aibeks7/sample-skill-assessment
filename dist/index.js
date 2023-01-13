4d9ec898631aeaebc2f0f619f5712d044130d6ba
GAME-PC\User <aibek.smagulov@toptal.com>
2023-01-13 21:52:37 +0600

    added sidebar

--- dist/index.js
+++ dist/index.js
@@ -11,13 +11,9 @@ const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.set('view engine', 'ejs');
 app.use('/assets', express_1.default.static(path_1.default.join(__dirname, '../public')));
-// index page
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Express + TypeScript Server');
-// });
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}. Cool`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}.`);
 });
--- index.ts
+++ index.ts
@@ -11,17 +11,10 @@ app.set('view engine', 'ejs');
 
 app.use('/assets', express.static(path.join(__dirname, '../public')));
 
-// index page
 app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Express + TypeScript Server');
-// });
-
 app.listen(port, () => {
-  console.log(
-    `⚡️[server]: Server is running at http://localhost:${port}. Cool`
-  );
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}.`);
 });
